4320a3150217360095af802859abde56e1d7dcff
shmamatkulov <mamatkulovs77@gmail.com>
2024-12-16 12:38:16 +0300

    upd 1 method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,23 +1,61 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//исключения для неверного размера массива
+class MyArraySizeException extends Exception {
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+}
+
+//исключение для неверных данных в массива
+class MyArrayDataException extends Exception{
+    public MyArrayDataException(String message) {
+        super(message);
+    }
+}
+
+class ArrayProcessor {
+    //метод для обработки массива
+    public static int processArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
+        //проверка размера массива
+        if (array.length !=4 || array[0].length != 4) {
+            throw new MyArraySizeException("массив должен быть 4х4");
+        }
+
+        int sum  = 0;
+
+        //
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                try {
+                    //преобразование строки в число и сумму
+                    sum += Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Неверные данные в ячейке["+ i + "][" + j + "]: " + array[i][j]);
+                }
+            }
+        }
+        return sum;
+    }
+}
+
 public class Main {
-    byte number = 120;
-    short number1 = 321;
-    int number2 = 5435;
-    long number3 = 2343242;
-
-
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-        //в блоку порполрпав
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //пример массива
+        String[][] array = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
+
+        try {
+            int result = ArrayProcessor.processArray(array);
+            System.out.println("Сумма всех элементов массива:" + result);
+        } catch (MyArraySizeException e) {
+            System.out.println("ошибка размера массива: " +e.getMessage());
+        } catch (MyArrayDataException e) {
+            System.out.println("Ошибка данных массива: " + e.getMessage());
         }
     }
 }
